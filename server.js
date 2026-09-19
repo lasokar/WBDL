0d3a8a2c1622344a28afa79b7715856f7a59f6de
Lasokar <sebaskarGD@gmail.com>
2026-09-18 18:55:00 -0700

    Make sessions last 2 weeks instead of 1 day

--- server.js
+++ server.js
@@ -1853,7 +1853,7 @@ const session = require('cookie-session');
 const sessionConfig = {
   name: 'session',
   keys: [process.env.SESSION_SECRET],
-  maxAge: 24 * 60 * 60 * 1000,
+  maxAge: 2 * 7 * 24 * 60 * 60 * 1000,
   sameSite: 'lax'
 };
 
